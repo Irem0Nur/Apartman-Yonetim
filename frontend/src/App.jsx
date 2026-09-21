6ee93ff32c58cb76c92bd3c55235dbb583223454
Irem0Nur <irem.nurr.celik@gmail.com>
2026-09-21 15:42:38 +0300

    Restore application routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,22 @@ import {
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import VerifyEmail from "./pages/VerifyEmail";
+
 import Dashboard from "./pages/Dashboard";
 import CreateApartment from "./pages/CreateApartment";
+import ApartmentInfo from "./pages/ApartmentInfo";
+
 import Units from "./pages/Units";
 import People from "./pages/People";
 import Dues from "./pages/Dues";
+import Payments from "./pages/Payments";
+
+import IncomeExpense from "./pages/IncomeExpense";
+import Cash from "./pages/Cash";
+import Decisions from "./pages/Decisions";
+import Meetings from "./pages/Meetings";
+
 import BusinessLedger from "./pages/BusinessLedger";
 
 
@@ -20,41 +31,90 @@ function App() {
 
       <Routes>
 
+        {/* Giriş */}
         <Route
           path="/"
           element={<Login />}
         />
 
+        {/* Kayıt */}
         <Route
           path="/register"
           element={<Register />}
         />
 
+        {/* E-posta doğrulama */}
+        <Route
+          path="/verify-email"
+          element={<VerifyEmail />}
+        />
+
+        {/* Ana sayfa */}
         <Route
           path="/dashboard"
           element={<Dashboard />}
         />
 
+        {/* Apartman */}
         <Route
           path="/apartman-olustur"
           element={<CreateApartment />}
         />
 
         <Route
+          path="/apartman-bilgileri"
+          element={<ApartmentInfo />}
+        />
+
+        {/* Daireler */}
+        <Route
           path="/daireler"
           element={<Units />}
         />
 
+        {/* Kişiler */}
         <Route
           path="/kisiler"
           element={<People />}
         />
 
+        {/* Aidatlar */}
         <Route
           path="/aidatlar"
           element={<Dues />}
         />
 
+        {/* Ödemeler */}
+        <Route
+          path="/odemeler"
+          element={<Payments />}
+        />
+
+        {/* Gelir / Gider */}
+        <Route
+          path="/gelir-gider"
+          element={<IncomeExpense />}
+        />
+
+        {/* Kasa */}
+        <Route
+          path="/kasa"
+          element={<Cash />}
+        />
+
+        {/* Kararlar */}
+        <Route
+          path="/kararlar"
+          element={<Decisions />}
+        />
+
+        {/* Toplantılar */}
+        <Route
+          path="/toplantilar"
+          element={<Meetings />}
+        />
+
+        {/* İşletme Defteri */}
         <Route
           path="/isletme-defteri"
           element={<BusinessLedger />}
